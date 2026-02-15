b4ea66b7c4c4ac294ed4e8bfb935473380841a87
unknown <omsatapathy7@gmail.com>
2026-02-15 16:40:32 +0530

    new code added

--- src/main/java/Feb14/DigitSum.java
+++ src/main/java/Feb14/DigitSum.java
@@ -1,5 +1,7 @@
 package Feb14;
 
+import java.util.Random;
+
 public class DigitSum {
 
 	public static void main(String[] args) {
@@ -9,6 +11,23 @@ public class DigitSum {
 
 		sum = n * (n + 1) / 2;
 		System.out.println(sum);
+
+		// leap year
+
+		int yr = 2016;
+
+		if (yr % 4 == 0 && yr % 100 != 0) {
+			System.out.println("leap yr");
+		} else {
+			System.out.println("not a leap yr");
+		}
+		
+		// generate random number
+		
+		Random rm = new Random();
+		for(int i =0; i<2; i++) {
+			System.out.println(rm.nextInt(200));
+		}
 
 	}
 
