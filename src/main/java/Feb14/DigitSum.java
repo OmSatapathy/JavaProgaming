46fbcaae0f40c4bde2e1b3222d35deaaedfe0610
unknown <omsatapathy7@gmail.com>
2026-02-15 16:30:59 +0530

    new dsa added

---
+++ src/main/java/Feb14/DigitSum.java
@@ -0,0 +1,15 @@
+package Feb14;
+
+public class DigitSum {
+
+	public static void main(String[] args) {
+		int n = 8;
+
+		int sum = 0;
+
+		sum = n * (n + 1) / 2;
+		System.out.println(sum);
+
+	}
+
+}
